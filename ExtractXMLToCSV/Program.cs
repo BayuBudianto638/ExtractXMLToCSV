0222de520709b3b28bbc9f1467d1d0353ee27abd
Bayu Hyuuga <radamanthys_ok@yahoo.com>
2024-09-27 13:26:39 +0700

    Add project files.

---
+++ ExtractXMLToCSV/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+class Program
+{
+    static void Main()
+    {
+        string xmlFilePath = "your_xml_file.xml";
+        string csvFilePath = "output.csv";
+
+        try
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(xmlFilePath);
+
+            using (StreamWriter writer = new StreamWriter(csvFilePath))
+            {
+                writer.WriteLine("lob,ruleId,defaultName,modelDesc,ruleType,isForDisplay,mainEntityType,maxScore,scoreCalcMethod,isRecurrenceByMonth,...");
+
+                foreach (XmlNode node in xmlDoc.DocumentElement.SelectNodes("//rule"))
+                {
+                    string lob = node.SelectSingleNode("lob").InnerText;
+                    string ruleId = node.SelectSingleNode("id").InnerText;
+
+                    writer.WriteLine($"{lob},{ruleId},{...}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
